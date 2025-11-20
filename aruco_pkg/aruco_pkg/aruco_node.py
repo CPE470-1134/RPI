75516f116062675fc92a4ae1d4567bc7b3970c58
Cpe470.1134 <cpe470.1134@gmail.com>
2025-11-20 04:27:54 -0800

    Resolved stash merge: recovered aruco_node + ignore fixes

--- aruco_pkg/aruco_pkg/aruco_node.py
+++ aruco_pkg/aruco_pkg/aruco_node.py
@@ -8,7 +8,8 @@ import cv2
 import numpy as np
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float32MultiArray
+#from std_msgs.msg import Float32MultiArray
+from aruco_pkg.msg import ArucoAlignment
 
 
 class ArucoAlignmentNode(Node):
@@ -27,18 +28,24 @@ class ArucoAlignmentNode(Node):
         self.declare_parameter("camera_width", 640)
         self.declare_parameter("camera_height", 480)
         self.declare_parameter("horizontal_fov_deg", 60.0)
-        self.declare_parameter("marker_length_m", 0.05)
+        self.declare_parameter("marker_length_m", 0.25) # Approx Standard Paper Size
         self.camera_index = int(self.get_parameter("camera_index").value)
         self.frame_width = int(self.get_parameter("camera_width").value)
         self.frame_height = int(self.get_parameter("camera_height").value)
         self.hfov_deg = float(self.get_parameter("horizontal_fov_deg").value)
         self.marker_length = float(self.get_parameter("marker_length_m").value)
+        
+        # --- Internal state -----------------------------------------------
         self.frame_num = 0
-
+        self.hit_count = 0
+        self.miss_count = 0
+        
         self.calib_path = self._locate_calibration_file()
-
+   
         # --- Load calibration ----------------------------------------------
         self.camera_matrix, self.dist_coeffs = self._load_calibration(self.calib_path)
+        self.aruco_frame_save_path = Path(__file__).resolve().parents[1] / "aruco_frames"
+        self.aruco_frame_save_path.mkdir(parents=True, exist_ok=True)
 
         # --- ArUco configuration ------------------------------------------
         self.aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
@@ -47,14 +54,25 @@ class ArucoAlignmentNode(Node):
 
         # --- Camera --------------------------------------------------------
         self.cap = cv2.VideoCapture(self.camera_index)
+        cap_w = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        cap_h = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.frame_width)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.frame_height)
 
         if not self.cap.isOpened():
             raise RuntimeError("Failed to open camera index {}".format(self.camera_index))
 
+        ## Camera opened successfully, Logging
+        self.get_logger().info(f"Camera opened: index={self.camera_index}, "
+                                 f"resolution={cap_w}x{cap_h}")
+        
         # --- Publisher -----------------------------------------------------
-        self.alignment_pub = self.create_publisher(Float32MultiArray, "aruco_alignment", 10)
+        self.alignment_pub = self.create_publisher(
+            ArucoAlignment,
+            "aruco_alignment",
+            10,
+        )
 
         # --- Timer ---------------------------------------------------------
         self.timer = self.create_timer(0.1, self._process_frame)  # 10 Hz
@@ -99,7 +117,7 @@ class ArucoAlignmentNode(Node):
     # MAIN PROCESS: Capture, Detect, Compute Pose
     # ============================================================
     def _process_frame(self) -> None:
-        
+        """Capture frame, detect ArUco marker, compute (alpha, delta), and publish."""   
 
         #self.get_logger().info(f"Reading Frame {self.frame_num}") 
         ret, frame = self.cap.read()
@@ -111,6 +129,10 @@ class ArucoAlignmentNode(Node):
 
         # Convert to grayscale for ArUco detection
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        
+        # Equalize image histogram to improve contrast between marker and background
+        #gray = cv2.equalizeHist(gray) # Did not see much improvement (Disable for now)
+        
         #cv2.imshow("Frame", gray)
         #cv2.waitKey(1)
 
@@ -120,11 +142,13 @@ class ArucoAlignmentNode(Node):
 
         # If nothing detected
         if ids is None or len(corners) == 0:
+            self.miss_count += 1
             self.get_logger().info(f"MISS: Frame {self.frame_num}")
             return
 
-        self.get_logger().info(f"HIT: Frame {self.frame_num}")
-        # Use *first* marker detected
+        self.hit_count += 1
+        
+        # Use *first* marker 
         marker_corners = corners[0]
         marker_id = ids[0][0]
 
@@ -132,12 +156,8 @@ class ArucoAlignmentNode(Node):
         # Compute alpha (bearing) USING PIXEL-BASED METHOD
         # -----------------------------------------------------------
         cX = int(np.mean(marker_corners[0][:, 0]))
-        cY = int(np.mean(marker_corners[0][:, 1]))
-
-        img_center_x = self.frame_width / 2.0
-        pixel_offset = cX - img_center_x
-        alpha_deg = (pixel_offset / self.frame_width) * self.hfov_deg
-        alpha_rad = math.radians(alpha_deg)
+        alpha_rad   = self.compute_alpha_pixel_based(cX=cX,marker_corners=marker_corners)
+        alpha_deg   = math.degrees(alpha_rad)
 
         # -----------------------------------------------------------
         # Compute delta USING PnP
@@ -169,10 +189,15 @@ class ArucoAlignmentNode(Node):
         delta = float(np.linalg.norm(tvec))
 
         # -----------------------------------------------------------
-        # Publish α, δ
+        # Make ArucoAlignment Message & Publish 
         # -----------------------------------------------------------
-        msg = Float32MultiArray()
-        msg.data = [float(alpha_rad), float(delta)]
+
+        msg = ArucoAlignment()
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.marker_id = int(marker_id)
+        msg.alpha = float(alpha_rad)
+        msg.distance = float(delta)
+
         self.alignment_pub.publish(msg)
 
         # -----------------------------------------------------------
@@ -181,40 +206,67 @@ class ArucoAlignmentNode(Node):
         #cv2.aruco.drawDetectedMarkers(frame, corners, ids)
         #cv2.drawFrameAxes(frame, self.camera_matrix, self.dist_coeffs, rvec, tvec, self.marker_length)
         #cv2.imshow("Aruco Alignment", frame)
-        #cv2.waitKey(1)
+        #cv2.waitKey(1) & 0xFF   
 
         self.get_logger().info(
-            f"[Frame {self.frame_num}] ID {marker_id} | "
-            f"alpha={alpha_rad:.4f} rad ({alpha_deg:.2f} deg) | "
-            f"delta={delta:.3f} m"
+            f"HIT: Frame {self.frame_num} | "
+            f"ID {marker_id} | "
+            f"alpha =({alpha_deg:.2f} deg) | "
+            f"delta =({delta:.2f}) m"
         )
-
+        
+        save_every_n_hits= 50
+        # Save every nth hit frame
+        #if self.hit_count % save_every_n_hits == 0:
+            #self.save_frame(frame, self.aruco_frame_save_path / f"aruco_id_{ids[0][0]}_delta_cm_{int(delta*100)}.png")
+        
+    # ============================================================
+    # Compute alpha using pixel-based method
+    # ============================================================
+    def compute_alpha_pixel_based(self,cX: int,  marker_corners: np.ndarray) -> float:
+        """
+        Compute alpha (bearing) based on pixel offset from image center.
+        Args:
+            cX (int): X-coordinate of the marker center in pixels.
+            marker_corners (np.ndarray): Corners of the detected marker.
+        """
+        img_center_x = self.frame_width / 2.0
+        pixel_offset = cX - img_center_x
+        alpha_deg = (pixel_offset / self.frame_width) * self.hfov_deg
+        alpha_rad = math.radians(alpha_deg)
+        return alpha_rad
+    
     # ============================================================
     # Cleanup
     # ============================================================
     def destroy_node(self) -> bool:
         if self.cap.isOpened():
             self.cap.release()
+        
+        self.get_logger().info(f"Total hits: {self.hit_count}, Total misses: {self.miss_count}")
+        self.get_logger().info(f"Hit Rate: {self.hit_count / (self.hit_count + self.miss_count):.2%}")
+        
         cv2.destroyAllWindows()
         return super().destroy_node()
 
-      
+    # ============================================================
+    # Helper Functions (Display, Save)
+    # ============================================================
     def display_frame(self, frame: np.ndarray) -> None:
-        
         cv2.imshow("Frame", frame)
         cv2.waitKey(1)
 
     def display_marker(self, frame: np.ndarray, corners: np.ndarray, ids: np.ndarray) -> None:
         cv2.aruco.drawDetectedMarkers(frame, corners, ids)
-        cv2.imshow("Frame", frame)
-        cv2.waitKey(5)
+        cv2.drawFrameAxes(frame, self.camera_matrix, self.dist_coeffs, rvec, tvec, self.marker_length)
+        cv2.imshow("Aruco Marker", frame)
+        cv2.waitKey(1) & 0xFF
         
-    # ----------------------------------------------------------------------
-    def destroy_node(self) -> bool:
-        if self.cap.isOpened():
-            self.cap.release()
-        return super().destroy_node()
-
+    def save_frame(self, frame: np.ndarray, filename: str) -> None:
+        cv2.imwrite(filename, frame)
+        cv2.waitKey(1)
+        self.get_logger().info(f"Saved frame to {filename}")
+        
 
 def main(args=None) -> None:
     rclpy.init(args=args)
@@ -222,6 +274,7 @@ def main(args=None) -> None:
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
+        node.get_logger().info("Keyboard interrupt, shutting down.")
         pass
     finally:
         node.destroy_node()
